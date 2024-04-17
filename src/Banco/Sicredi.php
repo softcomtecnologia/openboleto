23ebeabb26b0ab64bbf78941f12bb3f417862320
Arnaldo <arnaldogneto1@gmail.com>
2024-04-17 19:45:05 +0000

    Alterado o tipocobranca de 3 para 1 no banco do sicred

--- src/Banco/Sicredi.php
+++ src/Banco/Sicredi.php
@@ -98,7 +98,7 @@ class Sicredi extends BoletoAbstract {
      * Tipo de cobrança
      * @var int
      */
-    protected $tipoCobranca = 3;
+    protected $tipoCobranca = 1;
 
     /**
      * @param string|int $codigoCliente
